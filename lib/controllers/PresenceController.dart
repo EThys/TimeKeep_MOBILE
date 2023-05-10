7a55c67ac81680766b9173496b264fc84d1ca3ea
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-10 14:01:37 +0100

    feat: Création de notre controller pour la présence

---
+++ lib/controllers/PresenceController.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:squelette_mobile_parcours/utils/requetes.dart';
+
+class PresenceController with ChangeNotifier{
+  String ? wifiId;
+
+  setPresenceApi(String api_url,Map data) async{
+    var reponse=await postData(api_url,data);
+    print(reponse);
+
+  }
+}
+
+void main(){
+  var test=PresenceController();
+  test.setPresenceApi('/muzolaethberg/api_presence/presence.php',{'idwifi':'10:20:20:00'});
+}
