3edbc33d7b163bde216d3b15064eaab0b04370f9
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-18 16:32:52 +0100

    perf: Mise à niveau de mes requetes qui me permettront de communiquer avec mon api laravel

--- lib/utils/requetes.dart
+++ lib/utils/requetes.dart
@@ -3,43 +3,52 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:squelette_mobile_parcours/utils/Constantes.dart';
 
+class HttpResponse {
+  bool status;
+  Map? data;
+  String? errorMsg;
+  bool? isException;
 
-Future<dynamic> getData(String url_api) async{
+  HttpResponse(
+      {this.data, required this.status, this.errorMsg, this.isException});
+}
+
+Future<dynamic> getData(String url_api) async {
   try {
     var url = Uri.parse('${Constantes.BASE_URL}$url_api');
     var reponse = await http.get(url).timeout(Duration(seconds: 5));
-    print(reponse.runtimeType);
-    print(reponse.body.runtimeType);
-    print(reponse.body); // contient le resultat de la reponse
-    print(reponse.statusCode); // contient le code http
-    if(reponse.statusCode==200){
+
+    if (reponse.statusCode == 200) {
       return jsonDecode(reponse.body);
     }
     return null;
-  }catch(e, trace){
+  } catch (e, trace) {
     print(e.toString());
     print(trace.toString());
     return null;
   }
-
 }
 
-Future<dynamic> postData(String api_url,Map data) async{
+Future<HttpResponse> postData(String api_url, Map data) async {
   try {
-    var url = Uri.parse('${Constantes.BASE_URL}$api_url');
-    String dataStr = jsonEncode(data); //converti le data en chaine de caractere
-    var reponse = await http.post(url, body: dataStr).timeout(Duration(seconds: 5));
+    var url = Uri.parse("${Constantes.BASE_URL}$api_url");
+    String dataStr = json.encode(data);
+    var response = await http.post(url, body: dataStr, headers: {
+      "Content-Type": "application/json"
+    }).timeout(Duration(seconds: 5));
     var successList = [200, 201];
-    //print(reponse.body);
-    if (successList.contains(reponse.statusCode)) {
-      return json.decode(reponse.body);
-    }
-    return null;
-  }catch(e, trace){
+    var msg = json.decode(response.body);
+    var st = successList.contains(response.statusCode);
+
+    return HttpResponse(status: st, data: msg);
+
+  } catch (e, trace) {
     print(e.toString());
     print(trace.toString());
-    return null;
+
+    return HttpResponse(
+        status: false,
+        errorMsg: "Erreur inattendue",
+        isException: true);
   }
 }
-
-
