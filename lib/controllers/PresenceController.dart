208430345135d8ed5f3abda472ce6c2aab41aa57
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-18 16:34:38 +0100

    feat: Controller les actions reliant notre fichier requete et notre page de scanneur par qrcode

--- lib/controllers/PresenceController.dart
+++ lib/controllers/PresenceController.dart
@@ -4,14 +4,8 @@ import 'package:squelette_mobile_parcours/utils/requetes.dart';
 class PresenceController with ChangeNotifier{
   String ? wifiId;
 
-  setPresenceApi(String api_url,Map data) async{
+  Future<HttpResponse> setPresenceApi(String api_url,Map data) async{
     var reponse=await postData(api_url,data);
-    print(reponse);
-
+    return reponse;
   }
-}
-
- void main(){
-  var test=PresenceController();
-  test.setPresenceApi('/muzolaethberg/api_presence/presence.php',{'idwifi':'10:20:20:00'});
 }
