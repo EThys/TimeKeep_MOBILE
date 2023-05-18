2bc1ab4ce9d8f41bba6122a6a21e700051c0b5d8
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-18 16:29:49 +0100

    refactor: Modification du fichier constante en ajoutant la BASE_URL en fonction de mon adresse IP

--- lib/utils/Constantes.example.dart
+++ lib/utils/Constantes.example.dart
@@ -1,4 +1,4 @@
 class Constantes {
-  static const String BASE_URL = "http://VOTRE_IP";
+  static const String BASE_URL = "http://10.252.252.44:8000";
   static const String authentificationEndpoint = "/api/auth";
 }
