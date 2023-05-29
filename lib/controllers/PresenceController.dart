9e474a726b1b2c9d4b8cbba329c2cbc2042f0479
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-05-29 09:14:14 +0100

    refactor: Correction du controlleur selon les bonnes regles de codage

--- lib/controllers/PresenceController.dart
+++ lib/controllers/PresenceController.dart
@@ -6,6 +6,14 @@ class PresenceController with ChangeNotifier{
 
   Future<HttpResponse> setPresenceApi(String api_url,Map data) async{
     var reponse=await postData(api_url,data);
+    print('----------------------reponse ${reponse.data}');
+
     return reponse;
   }
 }
+
+
+ void main() {
+   var test = PresenceController();
+   test.setPresenceApi("/api/scanArrive", {"bssid":"02:00:00:00:00:00","user_id":"4"});
+ }
