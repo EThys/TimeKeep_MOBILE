c37411eea2ccb93de4e7a79ef09eeed231e7421e
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-06-02 16:19:06 +0100

    Refactor: Mise à jour en fonction du stockage local

--- lib/apps/MonApplication.dart
+++ lib/apps/MonApplication.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:squelette_mobile_parcours/controllers/PresenceController.dart';
 import '../utils/RoutesManager.dart';
-import 'package:provider/provider.dart';
 import '../utils/Routes.dart';
+import 'package:provider/provider.dart';
 
 class MonApplication extends StatelessWidget {
+  var _stockage = GetStorage();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: RoutesManager.route,
-        initialRoute: Routes.TemplateRoutes,
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(
+              create: (_) => PresenceController(stockage: _stockage)),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RoutesManager.route,
+          initialRoute: Routes.QrCodeScannerRoute,
+        ));
   }
 }
+
