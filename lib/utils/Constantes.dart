551876437ab299e74b4e71c188a69e62e09c9079
EthbergMuzola <ethbergmuzola500@gmail.com>
2023-06-09 08:51:13 +0100

    Refactor: Dernier mise à jour apres le premier test d'ensemble

--- lib/utils/Constantes.dart
+++ lib/utils/Constantes.dart
@@ -1,4 +1,4 @@
 class Constantes {
-  static const String BASE_URL = "http://10.252.252.30:8000";
-  static const String DefaultToken="33|7sAWq2c3qQYWuY8kPMLTl8eivXFAtKu6wMYq6JGt";
+  static const String BASE_URL = "http://10.252.252.5:8000";
+  static const String DefaultToken="4|xxdDrAVzHxe1eKVvECRM9Tv0LA8wqu8kF4BSM2bv";
 }
--- lib/utils/RoutesManager.dart
+++ lib/utils/RoutesManager.dart
@@ -8,23 +8,16 @@ import 'Routes.dart';
 class RoutesManager {
   static Route route(RouteSettings r) {
     switch (r.name) {
-      case Routes.TemplateRoutes:
-        //var args=r.arguments as String?;
-        return MaterialPageRoute(builder: (_) => TemplatePage());
+      case Routes.QrCodeScannerRoute:
         String _type=(r.arguments as String?) ?? "Entree"; //Sortie
-        return MaterialPageRoute(builder: (_) => QrPresenceScannerPage(type: _type,));
-        case Routes.AbsenceRoutes:
-        // return MaterialPageRoute(builder: (_) => AbsenceFormPage());
-        return MaterialPageRoute(builder: (_) => AbsencePage());
-      case Routes.HistoriqueRoutes:
-        return MaterialPageRoute(builder: (_) => HistoriquePage());
-      default:
-        return MaterialPageRoute(builder: (_) => StartedPage());
-
-      default:
-        String _type=(r.arguments as String?) ?? "Entree";
         return MaterialPageRoute(builder: (_) => QrPresenceScannerPage(type: _type));
->>>>>>> lib/utils/RoutesManager.dart
+        case Routes.AbsenceRoutes:
+          return MaterialPageRoute(builder: (_) => AbsencePage());
+          case Routes.HistoriqueRoutes:
+            return MaterialPageRoute(builder: (_) => HistoriquePage());
+            default:
+              return MaterialPageRoute(builder: (_) => StartedPage());
+
     }
   }
 }
